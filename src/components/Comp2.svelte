0fc2094474cbb3ff989e96f1c35a96679e5a76c9
Sveltecasts <sveltecasts@gmail.com>
2019-09-14 07:58:23 +0200

    init

---
+++ src/components/Comp2.svelte
@@ -0,0 +1,1 @@
+Hi from Comp 2
---
+++ src/routes/deep/deep/file.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Comp from "@components/Comp";
+  import Comp2 from "@components/Comp2";
+  import ErrorPage from "@routes/_error.svelte";
+</script>
+
+hello Component:
+<Comp />
+<br />
+Component 2
+<br />
+<Comp2 />
+<br />
+ErrorPage:
+<ErrorPage status="No real error" error={{ message: 'haha' }} />
